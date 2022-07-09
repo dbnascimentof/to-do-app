6c199919da429f34c02beb08c9e077272839b0cd
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-07-09 12:10:26 -0300

    Main_Screen from To-Do-App

--- app/src/main/java/br/com/danielfilho/todoapp/MainActivity.java
+++ app/src/main/java/br/com/danielfilho/todoapp/MainActivity.java
@@ -2,13 +2,25 @@ package br.com.danielfilho.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View btnPlus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        btnPlus = findViewById(R.id.btn_plus);
+
+        btnPlus.setOnClickListener(View ->{
+        Intent intent = new Intent(MainActivity.this, Page02_TO_DO_APP.class);
+        startActivity(intent);
+        });
+
+}
 }
---
+++ app/src/main/java/br/com/danielfilho/todoapp/Page02_TO_DO_APP.java
@@ -0,0 +1,14 @@
+package br.com.danielfilho.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Page02_TO_DO_APP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page02_to_do_app);
+    }
+}
